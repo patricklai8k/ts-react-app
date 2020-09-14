9328b538b2b6b390f99b6d4a5ec6778aae987f27
Patrick Lai <patrick.lai.8k@gmail.com>
2020-09-14 17:03:36 -0400

    first commit

---
+++ src/components/card/index.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react';
+
+type CardProps = {
+  title: string,
+  paragraph: string
+}
+
+export const Card: FunctionComponent<CardProps> = ({ title, paragraph }) => (
+  <aside>
+    <h2>{title}</h2>
+    <p>{paragraph}</p>
+  </aside>
+)
